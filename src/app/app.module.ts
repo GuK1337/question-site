38bd43e1b03f4dccebe13fbc7b2df1a23ff80802
Иван Корепанов <fbicha1234@gmail.com>
2021-12-13 18:08:24 +0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,61 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RouterModule, Routes} from "@angular/router";
+import {MatButtonModule} from '@angular/material/button';
+import { FirstTabComponent } from './tabs/first-tab/first-tab.component';
+import { MainComponent } from './tabs/main/main.component';
+import { StartPageComponent } from './start-page/start-page.component';
+import {MatTabsModule} from "@angular/material/tabs";
+
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo: '/main',
+    pathMatch: 'full'
+  },
+  {
+    path:'main',
+    component: StartPageComponent,
+  },
+  {
+    path:'tasks',
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/grammar'
+      },
+      {
+        path: 'grammar',
+        component: FirstTabComponent
+      }
+    ]
+  },
+  {
+    path:'**',
+    redirectTo: '/main'
+  }
+
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstTabComponent,
+    MainComponent,
+    StartPageComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTabsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
